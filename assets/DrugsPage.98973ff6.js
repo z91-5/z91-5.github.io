4c3685d9c3be5a98330525848b555456e372b92f
z91-5 <133259122+z91-5@users.noreply.github.com>
2023-05-12 01:29:35 +0300

    initial

---
+++ assets/DrugsPage.98973ff6.js
@@ -0,0 +1,1 @@
+import{Q as r}from"./QPage.7f124319.js";import{_ as a,U as e,V as o}from"./index.500caa22.js";import"./render.dcc885a8.js";const t={};function s(c,p,n,_,f,i){return e(),o(r,{padding:""})}var g=a(t,[["render",s]]);export{g as default};
